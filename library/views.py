80a3b725a047c089e663729282fde2bee760f62f
baratihd <barati1989@gmail.com>
2022-10-17 11:44:31 +0330

    create list view for showing list of books

--- library/views.py
+++ library/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import BookModel
+from index.mixins import LoginRequiredMixin
+
+
+class BookListView(LoginRequiredMixin, ListView):
+    template_name = 'library/book_list.html'
+    model = BookModel
+    context_object_name = 'books'
+    paginate_by = 10
