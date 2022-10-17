eb9150021eba0bca4b2b217ba713642c89b5f47a
baratihd <barati1989@gmail.com>
2022-10-17 14:56:31 +0330

    add registeration view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class RegisterUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super().save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
