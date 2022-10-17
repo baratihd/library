05c5f2f227140abc02c9e0b55df02d2573877cb1
baratihd <barati1989@gmail.com>
2022-10-17 22:08:00 +0330

    edit urls

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('/', include('index.urls')),
     path('library/', include('library.urls')),
-    path('/accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
